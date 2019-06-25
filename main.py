60466f6ec1b1be640b5746531a49290f238e720c
flukestar <43914450+flukestar@users.noreply.github.com>
2019-06-25 23:59:31 +0100

    changed filename

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import random
 import tim
+
 deck = [
     {"name": "prickler", "age": 10, "attack": 40, "speed": 38, "clever": 18},
     {"name": "ghost", "age": 10, "attack": 50, "speed": 10, "clever": 33},
